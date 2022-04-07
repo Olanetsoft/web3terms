97a4eb9eb5396d3ff382493b30f0ada1bedc602c
Idris Olubisi <heedris2olubisi@gmail.com>
2022-04-07 23:18:47 +0100

    Fix: Updated failure message

--- services/response.js
+++ services/response.js
@@ -1,8 +1,8 @@
 const failureMessage = () => {
   const texts = [
-    `Sorry😢, I don't have this term in my collection. I will update my collection soon.`,
-    `I would like to help but i can't at the moment. I will update my collection soon.`,
-    `Sorry, I don't know this term. I will update my collection soon.`,
+    `Sorry😢, I don't have this term in my collection. I'll be updating my collection shortly, or you can read my bio to learn more about how I reply to mentions.`,
+    `I would like to help but i can't at the moment. I'll be updating my collection shortly, or you can read my bio to learn more about how I reply to mentions.`,
+    `Sorry, I don't know this term. I'll be updating my collection shortly, or you can read my bio to learn more about how I reply to mentions.`,
   ];
 
   return texts[Math.floor(Math.random() * 3)];
