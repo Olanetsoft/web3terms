11720e0e4e40c4c408ecc4a0026374eefae99566
awojobigodfrey <67189904+awojobigodfrey@users.noreply.github.com>
2022-04-10 16:26:08 +0100

    Update response.js
    Added this incase u are adding to the failure msg, u don't have to count the text

--- services/response.js
+++ services/response.js
@@ -5,7 +5,7 @@ const failureMessage = () => {
     `Sorry, I don't know this term. I'll be updating my collection shortly, or you can read my bio to learn more about how I reply to mentions.`,
   ];
 
-  return texts[Math.floor(Math.random() * 3)];
+  return texts[Math.floor(Math.random() * text.length)];
 };
 
 export default failureMessage;
