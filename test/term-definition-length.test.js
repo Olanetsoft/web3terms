c3c9904afc497946340716310bec9b81da59dcd1
Qudusayo <qqudusayo@gmail.com>
2022-10-05 19:23:06 +0100

    feat: term definition length test

---
+++ test/term-definition-length.test.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+
+const __filename = path.resolve();
+
+const web3TermsPath = path.join(__filename, '/data/terms.json');
+
+describe('Check definition Length', () => {
+  // Read the file
+  const data = fs.readFileSync(web3TermsPath, 'utf8');
+
+  if (!data) {
+    return [];
+  }
+  const parsedData = JSON.parse(data);
+
+  parsedData.forEach(termObject => {
+    it(`"${termObject.term}" definition should be less than or equal 290, got ${termObject.meaning?.length}`, () => {
+      expect(termObject.meaning?.length).toBeLessThanOrEqual(290);
+    });
+  });
+});
